8ba384112bbe3b942c0665d435470d6fa4ccf8c2
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-06-13 14:51:29 +0200

    $0009: [Slot] [Logic]: start spinning routine 🎰

---
+++ blurred-lines-slot/Assets/Scripts/Slots.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slots : MonoBehaviour
+{
+    public Reel[] reels;
+    bool startSpin;
+
+    void Start()
+    {
+        startSpin = false;
+    }
+
+    void Update()
+    {
+        if (!startSpin) // prevents interference if the reel are still spinning 
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) //input that starts slot machine
+            {
+                startSpin = true;
+                StartCoroutine(Spinning());
+            }
+        }
+    }
+
+    IEnumerator Spinning()
+    {
+        foreach (Reel spinner in reels)
+        {
+            //tells each reel to start spinning
+            spinner.spin = true;
+        }
+        for (int i = 0; i < reels.Length; i++)
+        {
+            //allow the reel to spin for a random amount of time then stop
+            yield return new WaitForSeconds(Random.Range(1,3));
+            reels[i].spin = false;
+            reels[i].RandomPosition();
+        }
+        //allows the machine to be started again
+        startSpin = false;
+    }
+}
