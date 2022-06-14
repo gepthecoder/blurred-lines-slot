fcea14b55ba6ea3bc6a2719b495a18fa6bf42d47
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-06-14 11:03:16 +0200

    $0015: [Slot] [AutoSpins]: integrate auto spin mechanics

--- blurred-lines-slot/Assets/Scripts/AutoSpinsManager.cs
+++ blurred-lines-slot/Assets/Scripts/AutoSpinsManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 
 public class AutoSpinsManager : MonoBehaviour
 {
+    [SerializeField] private Slots slot_manager_;
+
     [SerializeField] private GameObject auto_spins_ui_;
 
     [SerializeField] private Button auto_play_btn_;
@@ -16,20 +18,42 @@ public class AutoSpinsManager : MonoBehaviour
 
     [SerializeField] private Button start_auto_spins_btn_;
 
+
+    [SerializeField] private Text auto_spin_counter_txt_;
+
     private bool auto_spins_shown_ = false;
+
     private int min_auto_spins_ = 10;
     private int max_auto_spins_ = 100;
     private int current_set_spins_;
 
+    private bool auto_spins_started_ = false;
+
     private void Awake()
     { 
+        //TODO: refac: uimanager->autospins
         auto_play_btn_.onClick.AddListener(OpenCloseAutoSpinsUi);
 
         increase_auto_spins_btn_.onClick.AddListener(IncreaseNumOfSpins);
         decrease_auto_spins_btn_.onClick.AddListener(DecreaseNumOfSpins);
-        start_auto_spins_btn_.onClick.AddListener(OpenCloseAutoSpinsUi);
+        start_auto_spins_btn_.onClick.AddListener(StartAutoSpins);
 
         current_set_spins_ = min_auto_spins_;
+        auto_spin_counter_txt_.text = "";
+    }
+
+    private void Update()
+    {
+        if (auto_spins_started_)
+        {
+            int auto_spins_left = slot_manager_.GetNumOfAutoSpinsLeft(); //TODO: expessive.. emmit event!
+            if(auto_spins_left <= 0) { 
+                auto_spins_started_ = false;
+                auto_spin_counter_txt_.text = "";
+
+            }
+            else { auto_spin_counter_txt_.text = $"{auto_spins_left}/{current_set_spins_}"; }
+        }
     }
 
     private void OpenCloseAutoSpinsUi()
@@ -54,8 +78,24 @@ public class AutoSpinsManager : MonoBehaviour
 
     private void StartAutoSpins()
     {
+        OpenCloseAutoSpinsUi();
 
+        //check balance
+
+        bool success;
+        slot_manager_.TryStartAutoSpins(current_set_spins_, out success);
+
+        if (success)
+        {
+            Debug.Log("Start auto spins: Success!!");
+            auto_spins_started_ = true;
+        }
+        else
+        {
+            Debug.Log("Spin is in progress: Failed!!");
+        }
     }
+
 
 
 }
--- blurred-lines-slot/Assets/Scripts/Slots.cs
+++ blurred-lines-slot/Assets/Scripts/Slots.cs
@@ -12,6 +12,10 @@ public class Slots : MonoBehaviour
     public Reel[] reels;
     bool startSpin;
 
+    private bool is_auto_play_;
+    private int num_auto_spins_;
+    public int GetNumOfAutoSpinsLeft() { return num_auto_spins_; }
+
     void Start()
     {
         spin_button_.onClick.AddListener(TryStartSpinning);
@@ -33,6 +37,8 @@ public class Slots : MonoBehaviour
 
     public void TryStartSpinning() // spin callback
     {
+        Debug.Log("TryStartSpinning");
+
         // prevents interference if the reel are still spinning
 
         if (!startSpin)
@@ -40,8 +46,68 @@ public class Slots : MonoBehaviour
             startSpin = true;
             spin_button_.interactable = false;
             spin_btn_anime_.SetTrigger(Consts.spin_button_trigger_);
+
             StartCoroutine(Spinning());
         }       
+    }
+
+    public void TryStartAutoSpins(int num_spins, out bool success)
+    {
+        if (!startSpin)
+        {
+            success = true;
+            Debug.Log("TryStartAutoSpins");
+            AutoSpins(num_spins);
+        }
+        else { 
+            success = false;
+        }
+    }
+
+    private void AutoSpins(int amount)
+    {
+        is_auto_play_ = true;
+
+        num_auto_spins_ = amount;
+        Debug.Log($"Num of auto spins set to {amount}");
+        // conduct for first spin
+        num_auto_spins_--;
+        startSpin = true;
+        spin_button_.interactable = false;
+        spin_btn_anime_.SetTrigger(Consts.spin_button_trigger_);
+        StartCoroutine(AutoSpinning(OnSpinFinnished));
+
+    }
+
+    private void OnSpinFinnished(string errorMessage)
+    {
+        if (errorMessage != null)
+        {
+            Debug.LogError("Was not able to execute spin: " + errorMessage);
+        }
+
+
+        if (is_auto_play_) { 
+
+            if(num_auto_spins_ <= 0) {
+                //stop loop 
+                is_auto_play_ = false;
+                num_auto_spins_ = 0;
+                spin_button_.interactable = true;
+                StopAllCoroutines();
+            }
+            else
+            {
+                if (!startSpin)
+                {
+                    num_auto_spins_--;
+                    startSpin = true;
+                    spin_button_.interactable = false;
+                    spin_btn_anime_.SetTrigger(Consts.spin_button_trigger_);
+                    StartCoroutine(AutoSpinning(OnSpinFinnished));
+                }
+            }
+        }
     }
 
     IEnumerator Spinning()
@@ -62,4 +128,26 @@ public class Slots : MonoBehaviour
         startSpin = false;
         spin_button_.interactable = true;
     }
+
+    private IEnumerator AutoSpinning(System.Action<string> onCompleted)
+    {
+        foreach (Reel spinner in reels)
+        {
+            //tells each reel to start spinning
+            spinner.spin = true;
+        }
+        for (int i = 0; i < reels.Length; i++)
+        {
+            //allow the reel to spin for a random amount of time then stop
+            yield return new WaitForSeconds(Random.Range(1, 3));
+            reels[i].spin = false;
+            reels[i].RandomPosition();
+        }
+        //allows the machine to be started again
+        startSpin = false;
+
+        yield return new WaitForSeconds(1f);
+        onCompleted(null);
+    }
+
 }
